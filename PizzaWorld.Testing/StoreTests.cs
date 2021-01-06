153dba5870c24cc321dbebd116d67e7c31b1f20e
Joshwin Greene <joshwin.greene@gmail.com>
2021-01-05 16:29:26 -0800

    Added the rest of the store tests

--- PizzaWorld.Testing/StoreTests.cs
+++ PizzaWorld.Testing/StoreTests.cs
@@ -18,5 +18,38 @@ namespace PizzaWorld.Testing
             Assert.IsType<Store>(actual);
             Assert.NotNull(actual);
         }
+
+        [Fact]
+        private void Test_CheckCreateOrder()
+        {
+            // arrange
+            var sut = new Store();
+
+            // act
+            sut.CreateOrder();
+            var actual = sut;
+
+            // assert
+            Assert.True(actual.Orders.Count == 1);
+        }
+
+        [Fact]
+        private void Test_CheckDeleteOrder()
+        {
+            // arrange
+            var sut = new Store();
+
+            sut.CreateOrder();
+            sut.CreateOrder();
+
+            var order = sut.Orders[1];
+
+            // act
+            var actual = sut.DeleteOrder(order);
+
+            // assert
+            Assert.True(actual == true);
+            Assert.True(sut.Orders.Count == 1);
+        }
     }
 }
